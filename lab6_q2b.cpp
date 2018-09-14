d23029989890312dd21ffd2dbf43e1bd15598b24
prasannajitsahoo <42238651+prasannajitsahoo@users.noreply.github.com>
2018-09-14 11:31:57 +0530

    Add files via upload

---
+++ lab6_q2b.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+//write a prograam with a function that takes 2 int parameter,adds them as void, takes a 3rd variables passed by reference and puts the sum in that
+void sum(int a, int b,int &c)
+{
+ c=a+b;
+}
+//The program should ask the user for 2 numbers,call the function with numbers as argument and tell the sum to the users
+int main()
+{
+int a,b,c;
+cout<<"Enter the first numbrer:"<<endl;
+cin>>a;
+cout<<"enter the second number:"<<endl;
+cin>>b;
+sum(a,b,c);//f(x)
+cout<<"The sum of two numbers : "<<c<<endl;
+return 0;
+}
